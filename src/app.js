84658d820db7bb435b196717109cef020561ff0e
Maxim Kovalov <max.kovalov@gmail.com>
2021-04-21 12:25:02 +0300

    feat: generate did doc

--- src/app.js
+++ src/app.js
@@ -9,16 +9,16 @@ var Entities = require('@selfkey/agent/lib/entities').default;
 var {createConnection} = require('typeorm');
 var kyccWebhookRouter = require('./routes/kycc-webhook');
 
+const dbConnection = createConnection({
+	type: 'sqlite',
+	database: config.dbName,
+	synchronize: true,
+	logging: ['error', 'info', 'warn'],
+	entities: Entities
+});
+
 var app = express();
 app.use((req, res, next) => {
-	const dbConnection = createConnection({
-		type: 'sqlite',
-		database: config.dbName,
-		synchronize: true,
-		logging: ['error', 'info', 'warn'],
-		entities: Entities
-	});
-
 	req.selfkeyAgent = new SelfkeyAgent({
 		dbConnection,
 		infuraId: config.infuraProjectId,
@@ -32,6 +32,17 @@ app.use((req, res, next) => {
 app.use(Sentry.Handlers.requestHandler());
 app.use(logger('dev'));
 app.use(express.json());
+
+app.get('/.well-known/did.json', async (req, res, next) => {
+	const did = await req.selfkeyAgent.ensureAgentDID();
+	try {
+		const doc = await req.selfkeyAgent.generateDIDDoc(did);
+		return res.json(doc);
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
+});
 
 app.get('/healthz', (req, res) => res.json({status: 'ok'}));
 
